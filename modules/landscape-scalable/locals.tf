61608be28eedd87610b2049ae9724fb78f586701
jansdhillon <jan.dhillon@canonical.com>
2025-10-17 14:24:51 -0600

    feat: update product module to use new Postgres charm interface

--- modules/landscape-scalable/locals.tf
+++ modules/landscape-scalable/locals.tf
@@ -2,5 +2,4 @@
 # See LICENSE file for licensing details.
 locals {
   self_signed = lookup(var.haproxy.config, "ssl_key", null) == null || lookup(var.haproxy.config, "ssl_cert", null) == null
-  legacy_amqp = var.landscape_server.channel != "latest-stable/edge" || (var.landscape_server.revision != null && var.landscape_server.revision <= 141)
 }
--- modules/landscape-scalable/main.tf
+++ modules/landscape-scalable/main.tf
@@ -67,7 +67,7 @@ resource "juju_integration" "landscape_server_inbound_amqp" {
 
   // Only run if Landscape Server is using the new AMQP relations
   // https://github.com/canonical/landscape-charm/blob/main/bundle-examples/bundle.yaml
-  count = local.legacy_amqp ? 0 : 1
+  count = try(module.landscape_server.requires.amqp) ? 0 : 1
 }
 
 resource "juju_integration" "landscape_server_outbound_amqp" {
@@ -84,7 +84,7 @@ resource "juju_integration" "landscape_server_outbound_amqp" {
 
   depends_on = [module.landscape_server, juju_application.rabbitmq_server]
 
-  count = local.legacy_amqp ? 0 : 1
+  count = try(module.landscape_server.requires.amqp) ? 0 : 1
 }
 
 # TODO: update when RMQ charm module exists
@@ -103,7 +103,7 @@ resource "juju_integration" "landscape_server_rabbitmq_server" {
 
   // Only run if Landscape Server is using the legacy AMQP relations
   // https://github.com/canonical/landscape-charm/blob/24.04/bundle-examples/bundle.yaml
-  count = local.legacy_amqp ? 1 : 0
+  count = try(module.landscape_server.requires.amqp) ? 1 : 0
 }
 
 resource "juju_integration" "landscape_server_haproxy" {
@@ -125,13 +125,13 @@ resource "juju_integration" "landscape_server_postgresql" {
 
   application {
     name     = module.landscape_server.app_name
-    endpoint = module.landscape_server.requires.db
+    endpoint = module.landscape_server.requires.database
   }
 
   application {
     # Output should be `app_name`, may have to change later when they comply
-    name     = module.postgresql.application_name
-    endpoint = "db-admin"
+    name = module.postgresql.application_name
+    endpoint = module.postgresql.provides.database
   }
 
 }
