ea428973dbac17fbd93f5c0394a040e62afe5d36
jansdhillon <jan.dhillon@canonical.com>
2025-11-11 13:57:05 -0700

    fmt

--- modules/landscape-scalable/versions.tf
+++ modules/landscape-scalable/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_version = ">= 1.10"
   required_providers {
     juju = {
-      source  = "juju/juju"
+      source = "juju/juju"
       # NOTE: 1.0 contains breaking changes that required modules have
       # yet to handle.
       version = "< 1.0.0"
